0965efc9a47023aa2236bf6e658c8ca43ebba068
Genevieve <gchiong@bren.ucsb.edu>
2022-04-09 21:35:48 -0700

    edited .R

--- R/almond_yield.R
+++ R/almond_yield.R
@@ -8,31 +8,18 @@
 #' @return yield anomaly (tons/acre)
 
 
-almond_yield = function(year, min_temp, precip, constant = 0.28) {
+almond_yield = function(clim, year, min_temp, precip, constant = 0.28) {
   
-  result = -0.015(min_temp) - 0.0046(min_temp^2) 
-  - 0.07(precip) + 0.0043(precip^2) + constant 
+  #filter(year = year)
+  #min_temp for month of february
+  #precip for month of january 
+
+  
+  result = (-0.015*min_temp) - (0.0046*min_temp^2) 
+  - (0.07*precip) + (0.0043*precip^2) + constant 
   
   return(result)
 }
-
-wheat_temperature = function(daily_temp, age, risk_threshold_med = 75, 
-                             risk_threshold_high = 82, age_threshold = 8) {
-  
-  daily_temp = as.data.frame(daily_temp)
-  
-  risk = 1.8 * daily_temp + 0.5 * (age^2)  
-  
-  # only high or med if wheat age is greater than 8 months 
-  if (any(age >= age_threshold)) {
-    risk = case_when(risk < risk_threshold_med ~ "low",
-                     risk >= risk_threshold_med &
-                       risk < risk_threshold_high ~ "medium", 
-                     risk >= risk_threshold_high ~ "high")  
-  } else
-    risk = "low" 
-
-
 
 
 
